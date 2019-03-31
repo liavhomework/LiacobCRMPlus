96cac1696ae94484a9051ab43c6c8bf5214c2b37
Liav Negrin <liav.negrin@gmail.com>
2019-03-31 14:05:44 +0300

    Create Initial infrastructure for client side

---
+++ LiacobCRMPlus/LiacobCRMPlus_Client/LiacobCRMPlus_Client/Components/TaskManagementComponet/TaskManagementComponetViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiacobCRMPlus_Client.Components
+{
+    public class TaskManagementComponetViewModel
+    {
+    }
+}
---
+++ LiacobCRMPlus/LiacobCRMPlus_Client/LiacobCRMPlus_Client/ViewModelsBase/ViewModelBase.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiacobCRMPlus_Client.ViewModelsBase
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged, IDisposable
+    {
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void OnPropertyChanged<T>(Expression<Func<T>> propertyExpression)
+        {
+            string propertyName = GetPropertyName(propertyExpression);
+            OnPropertyChanged(propertyName);
+        }
+
+        protected string GetPropertyName<T>(Expression<Func<T>> propertyExpression)
+        {
+            if (propertyExpression == null)
+            {
+                throw new ArgumentNullException("propertyExpression");
+            }
+
+            var body = propertyExpression.Body as MemberExpression;
+
+            if (body == null)
+            {
+                throw new ArgumentException("Invalid argument", "propertyExpression");
+            }
+
+            var property = body.Member as PropertyInfo;
+
+            if (property == null)
+            {
+                throw new ArgumentException("Argument is not a property", "propertyExpression");
+            }
+
+            return property.Name;
+        }
+
+        #endregion
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
