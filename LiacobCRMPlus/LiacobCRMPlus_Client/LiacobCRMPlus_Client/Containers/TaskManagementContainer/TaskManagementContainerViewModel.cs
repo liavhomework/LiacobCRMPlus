78bcec0c139e7aafec42b725149dc892ffef042c
Liav Negrin <liav.negrin@gmail.com>
2019-04-15 11:28:33 +0300

    fix code

--- LiacobCRMPlus/LiacobCRMPlus_Client/LiacobCRMPlus_Client/Containers/TaskManagementContainer/TaskManagementContainerViewModel.cs
+++ LiacobCRMPlus/LiacobCRMPlus_Client/LiacobCRMPlus_Client/Containers/TaskManagementContainer/TaskManagementContainerViewModel.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using LiacobCRMPlus_Client.Dialog;
+using LiacobCRMPlus_Client.ViewModelsBase;
+using MahApps.Metro.Controls.Dialogs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace LiacobCRMPlus_Client.Containers
 {
-    public class TaskManagementContainerViewModel
+    public class TaskManagementContainerViewModel: ViewModelBase
     {
+        private ResourceDictionary DialogDictionary = new ResourceDictionary() { Source = new Uri("pack://application:,,,/MaterialDesignThemes.MahApps;component/Themes/MaterialDesignTheme.MahApps.Dialogs.xaml") };
+
+        public ICommand NewTaskDialogCommand { get; }
+
+        public TaskManagementContainerViewModel()
+        {
+            NewTaskDialogCommand = new AnotherCommandImplementation(_ => InputDialog());
+        }
+
+        private void InputDialog()
+        {
+            var metroDialogSettings = new MetroDialogSettings
+            {
+                NegativeButtonText = "CANCEL"
+            };
+
+            DialogCoordinator.Instance.ShowInputAsync(this, "MahApps Dialog", "Using Material Design Themes");
+        }
     }
 }
